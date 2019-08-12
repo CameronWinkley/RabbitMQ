8470a85c132002f79f2f867e639b187b48319827
Cameron Winkley <cameron.winkley@gmail.com>
2019-08-12 08:21:14 -0600

    Update Receive.java

--- Receive.java
+++ Receive.java
@@ -1,9 +1,11 @@
+import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DeliverCallback;
 
-public class Receive {
+public class Recv {
+
     private final static String Queue_Name = "hello";
 
     public static void main(String[] argv) throws Exception{
@@ -12,7 +14,15 @@ public class Receive {
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
 
-        channel.queueDeclare(Queue_Name, false, false, false, null);
-        System.out.println(" [*] waiting for messages. to exit press CTRL+C");
+        channel.queueDeclare(Queue_Name, false, false,false,null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new STring(delivery.getBody(), "UTF-8");
+            System.out.println(" [x] Recieved '" + message + "'");
+
+        };
+        channel.basicConsume(Queue_Name, true, deliverCallback, consumertTag -> { });
+
     }
 }
